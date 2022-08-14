70c5a2b1d7e3236fde840498209cce5d2d94ea16
Rahul Thorat <61758268+RaThorat@users.noreply.github.com>
2022-08-14 20:11:24 +0200

    Update and rename beautifulsoup_for_info_extraction.py to web_scrapping.py

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,4 +1,4 @@
-#beautifulsoup to extract information from a website
+# method 1 beautifulsoup to extract information from a website
 import requests
 from bs4 import BeautifulSoup
 
@@ -26,3 +26,12 @@ for t in text:
         output += '{} '.format(t)
 
 print(output)
+
+# method 2 extract tables from wikipedia
+from pandas.io.html import read_html
+page = 'https://en.wikipedia.org/wiki/List_of_Asian_countries_by_area'
+
+wikitables = read_html(page,  attrs={"class":"wikitable"})
+
+print ("Extracted {num} wikitables".format(num=len(wikitables)))
+
